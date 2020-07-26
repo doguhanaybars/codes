2c68b12a4517e50ad3501124a927ec7d4259d914
doguhanaybars <doguhanaybars71@gmail.com>
2020-07-26 14:45:02 +0300

    master

---
+++ pythonCodes/pyqt5/creatingapp.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5 import QtWidgets
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    win = QtWidgets.QMainWindow()
+
+    win.show()
+    sys.exit(app.exec_())
+window()
