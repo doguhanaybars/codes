f986a3e59b8855b0c8cdc73c34f0e75aba1ac79e
doguhanaybars <doguhanaybars71@gmail.com>
2020-07-26 16:00:30 +0300

    master

--- pythonCodes/pyqt5/creatingapp.py
+++ pythonCodes/pyqt5/creatingapp.py
@@ -1,9 +1,16 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5 import QtGui
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
     win = QtWidgets.QMainWindow()
+
+    win.setWindowTitle('First Application')
+    win.setGeometry(200,200,1500,1500)
+    win.setWindowIcon(QtGui.QIcon('icon.png'))
+    win.setToolTip('my tool tip')
+    
 
     win.show()
     sys.exit(app.exec_())
---
+++ pythonCodes/pyqt5/winitems.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    win = QtWidgets.QMainWindow()
+    
+    win.setWindowTitle('First Application')
+    win.setGeometry(200,200,1500,1500)
+    win.setWindowIcon(QtGui.QIcon('icon.png'))
+    win.setToolTip('my tool tip')
+    
+    lbl_name = QtWidgets.QLabel(win)
+    lbl_name.setText('Adınız')
+    lbl_name.move(50,30)
+
+    lbl_surname = QtWidgets.QLabel(win)
+    lbl_surname.setText('Soyadınız')
+    lbl_surname.move(50,70)
+
+    txt_name = QtWidgets.QLineEdit(win)
+    txt_name.move(150 ,30)
+
+    txt_surname = QtWidgets.QLineEdit(win)
+    txt_surname.move(150 ,70)
+    
+    def clicked(self):
+        print('butona tıklandı name: ' + txt_name.text() + ' surname: ' + txt_surname.text())
+
+    btn_save = QtWidgets.QPushButton(win)
+    btn_save.setText('Kaydet')
+    btn_save.move(150,110)
+    btn_save.clicked.connect(clicked)
+    
+    win.show()
+    sys.exit(app.exec_())
+window()
