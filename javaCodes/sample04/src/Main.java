f23c4cd5cbe13435e667452c8e9434167c6c1864
doguhanaybars <doguhanaybars71@gmail.com>
2020-08-04 12:56:40 +0300

    yeni girdiler

---
+++ javaCodes/sample04/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sayi = 20;
+        if(sayi<20){
+            System.out.println("SAYI 20 DEN KÜÇÜKTÜR!");
+        }
+        else if(sayi==20){
+            System.out.println("SAYI 20 YE EŞİTTİR!");
+        }
+        else{
+            System.out.println("SAYI 20 DEN BÜYÜKTÜR!");
+        }
+
+
+
+
+
+
+
+    }
+}
